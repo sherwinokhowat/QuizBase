37c325f85a403697dc3c3ea2e3d24a52480da2a6
This-Username-Is-Missing <75860945+poweredbypigeon@users.noreply.github.com>
2023-05-30 09:36:02 -0400

    some code related to server - still need to figure out

--- web/Server.java
+++ web/Server.java
@@ -9,6 +9,11 @@ public class Server {
 
     private QuizManager quizManager;
 
+    private ServerSocket serverSocket;// server socket for connection
+
+    private static boolean running = true;  // controls if the server is accepting clients
+    private static boolean accepting = true;
+
     /**
      * Starts this server.
      *
@@ -18,5 +23,29 @@ public class Server {
     public void start(int port, String dbName) {
         userManager = new UserManager(dbName);
         quizManager = new QuizManager(dbName);
+        System.out.println("Waiting for a client connection..");
+
+        Socket client = null;//hold the client connection
+
+        try {
+            serverSocket = new ServerSocket(5000);  //assigns an port to the server
+            // serverSocket.setSoTimeout(15000);  //5 second timeout
+            while(accepting) {  //this loops to accept multiple clients
+                client = serverSocket.accept();  //wait for connection
+                System.out.println("Client connected");
+                //Note: you might want to keep references to all clients if you plan to broadcast messages
+                //Also: Queues are good tools to buffer incoming/outgoing messages
+                Thread t = new Thread(new ConnectionHandler(client)); //create a thread for the new client and pass in the socket
+                t.start(); //start the new thread
+            }
+        }catch(Exception e) {
+            System.out.println("Error accepting connection");
+            try {
+                client.close();
+            }catch (Exception e1) {
+                System.out.println("Failed to close socket");
+            }
+            System.exit(-1);
+        }
     }
 }
