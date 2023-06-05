287d12854b3cc5af4146ce17b2878b312829a756
Sherwin Okhowat <okthatssherwin@gmail.com>
2023-06-05 09:22:15 -0400

    testing
    uploading the rough version of the User class

---
+++ struct/User.java
@@ -0,0 +1,88 @@
+package struct;
+
+import java.util.Set;
+import java.util.HashSet;
+
+/**
+ * A class that represents a User object in the program
+ * 
+ * @author Sherwin Okhowat
+ */
+public class User {
+    private String username;
+    private UserManager manager;
+    private Set<Quiz> likedQuizzes;
+
+    /**
+     * Default constructor for a user
+     * 
+     * @param username the user's username
+     */
+    public User(String username) {
+        throw new UnsupportedOperationException("Unimplemented constructor 'User'");
+    }
+
+     // ------------------------ Functionality Methods ----------------------------
+
+    /**
+     * Method to add a quiz to liked quizzes collection
+     * 
+     * @param quiz the quiz to add
+     */
+    public void addLikedQuiz(Quiz quiz) {
+        this.likedQuizzes.add(quiz);
+    }
+
+    /**
+     * Method to remove a quiz from the liked quizzes collection
+     * 
+     * @param quiz the quiz to remove
+     */
+    public void removeLikedQuiz(Quiz quiz) {
+        this.likedQuizzes.remove(quiz);
+    }
+
+     // ------------------------ Setter Methods ----------------------------
+
+    /**
+     * Sets the username to a new username
+     * 
+     * @param username the new username
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+     // ------------------------ Getter Methods ----------------------------
+
+    /**
+     * Returns the username
+     * 
+     * @return the username
+     */
+    public String getUsername() {
+        return this.username;
+    }
+
+    /**
+     * Getter for the collection of liked quizzes
+     * 
+     * @return the collection of liked quizzes
+     */
+    public Set<Quiz> getLikedQuizzes() {
+        return this.likedQuizzes;
+    }
+
+    // ------------------------ Database Methods ----------------------------
+
+    /**
+     * Retrieves the liked quizzes from the database
+     * 
+     * @return whether the retrieval was successful or not
+     */
+    private boolean getLikedQuizzesFromDB() {
+        throw new UnsupportedOperationException("Unimplemented constructor 'getLikedQuizzesFromDB'");
+    }
+
+
+}
