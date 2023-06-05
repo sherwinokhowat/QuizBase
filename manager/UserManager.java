91893ab0c43157f09b35c6002417e4c28206e100
rickyqin005 <69991001+rickyqin005@users.noreply.github.com>
2023-06-05 10:05:49 -0400

    added authenticateUser method

--- manager/UserManager.java
+++ manager/UserManager.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import struct.User;
 
 import utility.Pair;
+import utility.SQLStatementBuilder;
 
 /**
  * A class that manages {@code User} objects
@@ -16,6 +17,11 @@ import utility.Pair;
  */
 public class UserManager extends DatabaseManager {
 
+    /**
+     * Constructs a UserManager class for the specified database
+     *
+     * @param dbName The name of database to manage (must include {@code .db} file extension)
+     */
     public UserManager(String dbName) {
         super(dbName);
         //TODO Auto-generated constructor stub
@@ -28,14 +34,31 @@ public class UserManager extends DatabaseManager {
             ResultSet rs = result.first();
             ArrayList<User> list = new ArrayList<User>();
             while(rs.next()) {
-                int id = rs.getInt("id");
-                String username = rs.getString("username");
-                list.add(new User(username));
+                int id = rs.getInt("ID");
+                String username = rs.getString("USERNAME");
+                list.add(new User(id, username, this));
             }
             result.second().close();
             return list;
         } catch(SQLException e) {
             e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Authenticates a user by checking the provided credentials against the database.
+     *
+     * @param username The username
+     * @param password The password
+     * @return The User object or {@code null} if the credentials do not match
+     */
+    public User authenticateUser(String username, String password) {
+        ArrayList<? extends Object> list = executeReadOperation(new SQLStatementBuilder().select()
+                .from("USERS").where("USERNAME="+username+" AND PASSWORD="+password).toString());
+        if(list != null) {
+            return (User)(list.get(0));
+        } else {
             return null;
         }
     }
