261811ab9cc047c9495d846b67920833394f9a0a
Sherwin Okhowat <106927080+sherwinokhowat@users.noreply.github.com>
2023-06-05 22:27:38 -0400

    slight modifications

--- struct/Quiz.java
+++ struct/Quiz.java
@@ -5,68 +5,115 @@ import manager.QuizManager;
 
 public class Quiz {
     private PriorityQueue<QuizItem> quizItems;
-    private int likes;
     private String name;
     private String description;
     private int id;
+    private int likes;
     private User creator;
-    private HashMap<User, Integer> leaderboard;
-    private QuizManager quizManager;
+    private QuizManager manager;
 
-    public Quiz(QuizManager q) {
-        this.quizManager = q;
+    public Quiz(QuizManager manager) {
+        this.manager = manager;
         // unimplemented
     }
 
+    /**
+     * Returns the number of likes associated with this quiz
+     * 
+     * @return the number of likes
+     */
     public int getLikes() {
         return this.likes;
     }
 
+    /**
+     * Method for adding a like to a quiz
+     * 
+     * @param user the user liking the quiz
+     */
     public void addLike(User user) {
         this.likes++;
-        // unimplemented
+        throw new UnsupportedOperationException("Unimplemented method 'addLike'");
     }
 
+    /**
+     * Returns the name of the quiz
+     * 
+     * @return the name of the quiz
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Sets the name of the quiz to a new name
+     * 
+     * @param name the new name of the quiz
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Returns the description of the quiz
+     * 
+     * @return the description of the quiz
+     */
     public String getDescription() {
         return this.description;
     }
 
+    /**
+     * Sets the description of the quiz
+     * 
+     * @param description the new description of the quiz
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Returns the id of the quiz
+     * 
+     * @return the unique id of the quiz
+     */
     public int getID() {
         return this.id;
     }
 
+    /**
+     * Sets the id of the quiz
+     * 
+     * @param id the id of the quiz
+     */
     public void setID(int id) {
         this.id = id;
     }
 
+    /**
+     * Returns the creator of the quiz
+     * 
+     * @return the creator of the quiz
+     */
     public User getCreator() {
         return this.creator;
     }
 
     /**
-     * +addItem(QuizItem q) : void - requires database connection
-+ removeItem(QuizItem q) : void - requires database connection
-+ setName (String newName) boolean - database connection
-+ setDescription (String newDescription) boolean - database connection
+     * Adds a quiz item to the quiz
+     * 
+     * @param item the item to be added to the quiz
      */
-
      public void addItem(QuizItem item) {
          this.quizItems.add(item);
         // unimplemented
      }
 
+     /**
+      * Removes a quiz item from the quiz
+      *
+      * @param item the item to remove from the quiz
+      */
      public void removeItem(QuizItem item) {
          this.quizItems.remove(item);
      }     
