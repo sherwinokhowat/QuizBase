015c9a52d56111273a762dd8344ed63880a4b907
Sherwin Okhowat <106927080+sherwinokhowat@users.noreply.github.com>
2023-06-06 00:17:43 -0400

    first iteration of the quizitem class

---
+++ struct/QuizItem.java
@@ -0,0 +1,63 @@
+package struct;
+
+public abstract class QuizItem implements Comparable<QuizItem> {
+    private int id;
+    private int frequency;
+
+    public QuizItem(int id, int frequency) {
+        this.id = id;
+        this.frequency = frequency;
+    }
+
+    /**
+     * Increasing the frequency of this quiz item. If the user gets this quiz item
+     * wrong, it will increase the frequency in which this quiz item appears
+     */
+    public void increaseFrequency() {
+        this.frequency++;
+    }
+
+    /**
+     * Returns the quiz item's id
+     * 
+     * @return the id of this quiz item
+     */
+    public int getId() {
+        return this.id;
+    }
+
+    /**
+     * Sets the quiz item's id
+     * 
+     * @param id the quiz item's id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Returns this quiz item's frequency
+     * 
+     * @return this quiz item's frequency
+     */
+    public int getFrequency() {
+        return this.frequency;
+    }
+
+    /**
+     * Method that defines the compare method as one which compares the frequencies
+     * 
+     * @param other the other quiz item being compared
+     * @return the order
+     */
+    @Override
+    public int compareTo(QuizItem other) {
+        if (this.getFrequency() < other.getFrequency()) {
+            return 1;
+        } else if (this.getFrequency() > other.getFrequency()) {
+            return -1;
+        }
+        return 0;
+    }
+
+}
