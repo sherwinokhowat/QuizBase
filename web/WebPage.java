16298c1b49d73a05d85158e4f07742534f143bc1
Sherwin Okhowat <okthatssherwin@gmail.com>
2023-06-07 09:32:29 -0400

    new class

---
+++ web/WebPage.java
@@ -0,0 +1,19 @@
+package web;
+
+/**
+ * 
+ * 
+ * @author Sherwin Okhowat and Ricky Qin
+ */
+public class WebPage implements WebComponent {
+
+    // one for login page
+    // one for sign in page
+    // one for list of quizzes page
+
+    @Override
+    public String toHTMLString() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
