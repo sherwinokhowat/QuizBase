1b89c09a076c3299f9f38f402c7571eca452e083
rickyqin005 <69991001+rickyqin005@users.noreply.github.com>
2023-06-07 18:15:21 -0400

    fixed Server class

--- web/Server.java
+++ web/Server.java
@@ -11,17 +11,19 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * The Server class, responsible for managing client and database connections.
+ *
+ * @author Ricky Qin, Avery Chan
+ */
 public class Server {
 
     private UserManager userManager;
 
     private QuizManager quizManager;
 
-    private ServerSocket serverSock;// server socket for connection
+    private ServerSocket serverSock;
 
-    private ArrayList<Thread> threadList;
-
-    // private static boolean running = true;  // controls if the server is accepting clients
     private static boolean accepting = true;
 
     /**
@@ -40,24 +42,28 @@ public class Server {
         Socket client = null;
 
         try {
-            serverSock = new ServerSocket(5000);  //assigns an port to the server
-            // serverSock.setSoTimeout(15000);  //5 second timeout
-            while(accepting) {  //this loops to accept multiple clients
-                client = serverSock.accept();  //wait for connection
-                System.out.println("Client connected");
-                //Note: you might want to keep references to all clients if you plan to broadcast messages
-                //Also: Queues are good tools to buffer incoming/outgoing messages
-                Thread t = new Thread(new ConnectionHandler(client)); //create a thread for the new client and pass in the socket
+            serverSock = new ServerSocket(5000);// assigns an port to the server
+            serverSock.setSoTimeout(0);// will never timeout while waiting for connections
+
+            while(accepting) {
+                System.out.println("["+Thread.currentThread()+"] "+"waiting for connection");
+                client = serverSock.accept();// wait for connection
+                System.out.println("["+Thread.currentThread()+"] "+"Client connected");
+
+                // Note: you might want to keep references to all clients if you plan to broadcast messages
+                // Also: Queues are good tools to buffer incoming/outgoing messages
+
+                Thread t = new Thread(new ConnectionHandler(client));// create a thread for the new client and pass in the socket
+                System.out.println("["+Thread.currentThread()+"] "+" about to start new thread");
                 t.start(); //start the new thread
 
-                // accepting = false;
             }
         } catch(Exception e) {
             System.out.println("Error accepting connection");
-            //close all and quit
+            // close all and quit
             try {
                 client.close();
-            }catch (Exception e1) {
+            } catch (Exception e1) {
                 System.out.println("Failed to close socket");
             }
             System.exit(-1);
@@ -67,21 +73,19 @@ public class Server {
     //***** Inner class - thread for client connection
     class ConnectionHandler implements Runnable {
 
-        private PrintWriter output; //assign printwriter to network stream
-        private BufferedReader input; //Stream for network input
-        private Socket client;  //keeps track of the client socket
+        private PrintWriter output;// assign printwriter to network stream
+        private BufferedReader input;// Stream for network input
+        private Socket client;// keeps track of the client socket
         private boolean running;
-
-        //HTML stuff ??
-
 
         /**
          * Constructs a ConnectionHandler
-         * @param s the socket belonging to this client connection
+         *
+         * @param sock the socket belonging to this client connection
          */
         ConnectionHandler(Socket sock) {
-            this.client = sock;  //constructor assigns client to this
-            try {  //assign all connections to client
+            this.client = sock;// constructor assigns client to this
+            try {// assign all connections to client
                 this.output = new PrintWriter(client.getOutputStream());
                 InputStreamReader stream = new InputStreamReader(client.getInputStream());
                 this.input = new BufferedReader(stream);
@@ -96,11 +100,13 @@ public class Server {
          */
         @Override
         public void run() {
+            System.out.println("["+Thread.currentThread()+"] "+" this thread just started");
             // Get a message from the client
             ArrayList<String> request = new ArrayList<>();
 
             // Get a message from the client, loops until a message is received
             while(running) {
+                System.out.println("["+Thread.currentThread()+"] "+"checking for response");
                 try {
                     // check for incoming responses
                     while(input.ready()) {
@@ -109,10 +115,12 @@ public class Server {
 
                     if(request.size() != 0) {
                         // process request here
-                        System.out.println(request);
+                        System.out.println("["+Thread.currentThread()+"] "+request.get(0));
                         processRequest(request);
                         request.clear();
+                        System.out.println("["+Thread.currentThread()+"] "+"processed request");
                     }
+                    running = false;
 
                 } catch (IOException e) {
                     System.out.println("Failed to receive msg from the client");
@@ -128,6 +136,7 @@ public class Server {
             } catch (Exception e) {
                 System.out.println("Failed to close socket");
             }
+            System.out.println("["+Thread.currentThread()+"] "+"Ending thread");
         }
 
         /**
@@ -140,19 +149,29 @@ public class Server {
                 String[] firstLine = request.get(0).split(" ");
                 // resource path is stored in firstLine[1]
                 String path = firstLine[1];
-                System.out.println(path);
 
                 if(path.equals("/favicon.ico")) {
-                    output.println("404 Not Found");
+                    output.println("HTTP/1.1 404 Not Found");
                     output.flush();
                     return;
                 }
+
                 StringBuilder content = new StringBuilder();
                 content.append("<html>");
                 content.append("<head>");
                 content.append("</head>");
                 content.append("<body>");
-                content.append(new Hyperlink("/login/", "Log in").toHTMLString());
+                if(path.equals("/")) {// homepage
+
+                    content.append(new Hyperlink("/login/", "Log in").toHTMLString());
+                    content.append(new Hyperlink("/signup/", "Sign up").toHTMLString());
+
+                } else if(path.equals("/login/")) {// login page
+                    content.append("<p>Username</p>");
+                    content.append("<p>Password</p>");
+                } else if(path.equals("/signup/")) {// signup page
+
+                }
                 content.append("</body>");
                 content.append("</html>");
 
