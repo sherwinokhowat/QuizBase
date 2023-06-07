755c59c5780a43cb15d441e6e9a49321bf5d59d2
rickyqin005 <69991001+rickyqin005@users.noreply.github.com>
2023-06-06 23:13:21 -0400

    updated Server

--- web/Server.java
+++ web/Server.java
@@ -4,7 +4,12 @@ import manager.QuizManager;
 import manager.UserManager;
 
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
 
@@ -12,11 +17,11 @@ public class Server {
 
     private QuizManager quizManager;
 
-    private ServerSocket serverSocket;// server socket for connection
+    private ServerSocket serverSock;// server socket for connection
 
     private ArrayList<Thread> threadList;
 
-    private static boolean running = true;  // controls if the server is accepting clients
+    // private static boolean running = true;  // controls if the server is accepting clients
     private static boolean accepting = true;
 
     /**
@@ -30,28 +35,26 @@ public class Server {
         quizManager = new QuizManager(dbName);
         userManager.connectToDatabase();
         quizManager.connectToDatabase();
-        /*
-        System.out.println("Waiting for a client connection..");
 
-
-        Socket client = null;//hold the client connection
-
-
+        // hold the client connection
+        Socket client = null;
 
         try {
-            serverSocket = new ServerSocket(5000);  //assigns an port to the server
-            // serverSocket.setSoTimeout(15000);  //5 second timeout
+            serverSock = new ServerSocket(5000);  //assigns an port to the server
+            // serverSock.setSoTimeout(15000);  //5 second timeout
             while(accepting) {  //this loops to accept multiple clients
-                client = serverSocket.accept();  //wait for connection
+                client = serverSock.accept();  //wait for connection
                 System.out.println("Client connected");
                 //Note: you might want to keep references to all clients if you plan to broadcast messages
                 //Also: Queues are good tools to buffer incoming/outgoing messages
                 Thread t = new Thread(new ConnectionHandler(client)); //create a thread for the new client and pass in the socket
-                threadList.add(t);
                 t.start(); //start the new thread
+
+                // accepting = false;
             }
-        }catch(Exception e) { // what are the exceptions here???
+        } catch(Exception e) {
             System.out.println("Error accepting connection");
+            //close all and quit
             try {
                 client.close();
             }catch (Exception e1) {
@@ -59,6 +62,94 @@ public class Server {
             }
             System.exit(-1);
         }
-        */
+    }
+
+    //***** Inner class - thread for client connection
+    class ConnectionHandler implements Runnable {
+
+        private PrintWriter output; //assign printwriter to network stream
+        private BufferedReader input; //Stream for network input
+        private Socket client;  //keeps track of the client socket
+        private boolean running;
+
+        //HTML stuff ??
+
+
+        /**
+         * Constructs a ConnectionHandler
+         * @param s the socket belonging to this client connection
+         */
+        ConnectionHandler(Socket sock) {
+            this.client = sock;  //constructor assigns client to this
+            try {  //assign all connections to client
+                this.output = new PrintWriter(client.getOutputStream());
+                InputStreamReader stream = new InputStreamReader(client.getInputStream());
+                this.input = new BufferedReader(stream);
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+            running = true;
+        }
+
+        /**
+         * Executed on start of thread
+         */
+        @Override
+        public void run() {
+            // Get a message from the client
+            ArrayList<String> request = new ArrayList<>();
+
+            // Get a message from the client, loops until a message is received
+            while(running) {
+                try {
+                    // check for incoming responses
+                    while(input.ready()) {
+                        request.add(input.readLine());
+                    }
+
+                    if(request.size() != 0) {
+                        // process request here
+                        System.out.println(request);
+                        processRequest(request);
+                        request.clear();
+                    }
+
+                } catch (IOException e) {
+                    System.out.println("Failed to receive msg from the client");
+                    e.printStackTrace();
+                }
+            }
+
+            // close the socket
+            try {
+                input.close();
+                output.close();
+                client.close();
+            } catch (Exception e) {
+                System.out.println("Failed to close socket");
+            }
+        }
+
+        /**
+         * Processes a request and sends a response to the client
+         *
+         * @param request The request, split by the line separator
+         */
+        private void processRequest(ArrayList<String> request) {
+            if(request.get(0).startsWith("GET")) {
+                String[] firstLine = request.get(0).split(" ");
+                // resource path is stored in firstLine[1]
+                System.out.println(firstLine[1]);
+
+                String content = "content goes here";
+                output.println("HTTP/1.1 200 OK");
+                output.println("Content-Type: text/html");
+                output.println("Content-Length: " + content.length());
+                output.println();
+                output.println(content);
+                output.flush();
+            }
+        }
+
     }
 }
