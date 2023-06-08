960fb43d51f94f447d1c63b305a92e69cf7bc946
Sherwin Okhowat <106927080+sherwinokhowat@users.noreply.github.com>
2023-06-08 00:41:45 -0400

    removed commented out code

--- web/WebPage.java
+++ web/WebPage.java
@@ -8,15 +8,4 @@ public abstract class WebPage implements WebComponent {
 
     // common functionality for all webpages
 
-//    @Override
-//    public String toHTMLString() {
-//        // TODO Auto-generated method stub
-//        return "<form action='/submit-signup' method='post'>"
-//                + "<label for='username'>Username:</label><br>"
-//                + "<input type='text' id='username' name='username'><br>"
-//                + "<label for='password'>Password:</label><br>"
-//                + "<input type='password' id='password' name='password'><br>"
-//                + "<input type='submit' value='Sign Up'>"
-//                + "</form>";
-//    }
 }
