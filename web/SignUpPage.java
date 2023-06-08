3a4f6d6e196d12ab8d690349a72f0d2abf7b0ee3
Sherwin Okhowat <106927080+sherwinokhowat@users.noreply.github.com>
2023-06-08 00:09:56 -0400

    intext css and logo
    logo doesnt load for some reason

--- web/SignUpPage.java
+++ web/SignUpPage.java
@@ -1,22 +1,26 @@
 package web;
 
+/**
+ * Class representing the signup page of the server
+ *
+ * @author Sherwin Okhowat
+ */
 public class SignUpPage extends WebPage {
     @Override
     public String toHTMLString() {
-        StringBuilder html = new StringBuilder();
-        html.append("<form action='/submit-signup' method='post'>");
-        html.append("<label for='username'>Username</label>");
-        html.append("<br>");
-        html.append("<input type='text' id='username' name='username'");
-        html.append("<br>");
-        html.append("<br>");
-        html.append("<label for='password'>Password</label>");
-        html.append("<br>");
-        html.append("<input type='password' id='password' name='password'");
-        html.append("<br>");
-        html.append("<br>");
-        html.append("<input type='submit' value='Sign Up'>");
-        html.append("</form>");
-        return html.toString();
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("<html>");
+        sb.append("<body style='background-color: lightblue; display: flex; flex-direction: column; justify-content: center; align-items: center;'>");
+        sb.append("<img src='images/logo.png' style='width: 400px; height: auto;'>");
+        sb.append("<form style='display: flex; flex-direction: column; width: 200px; margin-top: 50px; padding: 20px;'>");
+        sb.append("<input style='margin-bottom: 3px;' type='text' id='username' name='username' placeholder='Username'>");
+        sb.append("<input style='margin-bottom: 5px;' type='password' id='password' name='password' placeholder='Password'>");
+        sb.append("<input type='submit' value='Sign Up'>");
+        sb.append("</form>");
+        sb.append("</body>");
+        sb.append("</html>");
+
+        return sb.toString();
     }
 }
