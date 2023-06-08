18cb80245869e56e00b32886247c4a7a5949744c
rickyqin005 <69991001+rickyqin005@users.noreply.github.com>
2023-06-08 11:55:15 -0400

    removed debugging code

--- Main.java
+++ Main.java
@@ -1,16 +1,7 @@
-import utility.Pair;
-import utility.SQLStatementBuilder;
 import web.Server;
 
 public class Main {
     public static void main(String[] args) {
-        // TODO start program here
-        System.out.println(new SQLStatementBuilder().insertInto("QUIZZES").values("1", "Biology", "10", "2.5"));
-        System.out.println(new SQLStatementBuilder().select("ID", "NAME", "LENGTH", "DIFFICULTY").from("QUIZZES").where("LENGTH <= 10").limit(10));
-        System.out.println(new SQLStatementBuilder().select("ID", "NAME", "LENGTH", "DIFFICULTY").from("QUIZZES").orderBy(new Pair<>("DIFFICULTY", SQLStatementBuilder.DESCENDING), new Pair<>("NAME", SQLStatementBuilder.ASCENDING)));
-        System.out.println(new SQLStatementBuilder().select().from("USERS"));
-        System.out.println(new SQLStatementBuilder().update("QUIZZES").set(new Pair<>("DIFFULTY", "3.5")).where("ID = 1"));
-        System.out.println(new SQLStatementBuilder().deleteFrom("USERS").where("ID = 42"));
         new Server().start(3000, "storage.db");
     }
 }
