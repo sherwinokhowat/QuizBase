ee00218afcd974e8e75a3cff8a0e1d146585e551
This-Username-Is-Missing <averymachan@gmail.com>
2023-06-11 19:59:58 -0400

    editing image

--- web/path/QuizGetNextQuestion.java
+++ web/path/QuizGetNextQuestion.java
@@ -57,6 +57,7 @@ public class QuizGetNextQuestion extends WebPage implements HTTPPath {
             response.appendBody("No more questions left!");
             server.endQuiz(credentials.first(), quizID);
         }
+        response.appendBody(toHTMLString());
         return response;
     }
 
